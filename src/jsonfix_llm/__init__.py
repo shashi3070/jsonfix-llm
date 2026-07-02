de10f7af289b11facc8a6afffcdeb4b95fc10f3d
shashi kundan <rk683420@gmail.com>
2026-07-02 06:49:58 +0530

    v0.1.1: author metadata, expanded keywords, license-files, classifiers, documentation URLs

--- src/jsonfix_llm/__init__.py
+++ src/jsonfix_llm/__init__.py
@@ -1,3 +1,7 @@
+__version__ = "0.1.1"
+__author__ = "Shashi Kundan"
+__email__ = "shashikundan0001@gmail.com"
+
 from jsonfix_llm.extract.extract_code import extract_code
 from jsonfix_llm.extract.extract_json import extract_json
 from jsonfix_llm.repair.pipeline import repair_json
